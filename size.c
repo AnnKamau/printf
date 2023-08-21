efae0b65b4ee88957610f796fa2c395e4129f297
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2023-08-21 21:55:25 +0300

    Update size.c

--- size.c
+++ size.c
@@ -12,10 +12,10 @@ int get_size(const char *format, int *i)
 	int curr_i = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_i] == '1')
 		size = S_LONG;
 	else if (format[curr_i] == 'h')
-		size = S_SHORT;
+		size = s_SHORT;
 
 	if (size == 0)
 		*i = curr_i - 1;
