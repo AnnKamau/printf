5c2feaa4cf648e5d34404520417d70bacb83724c
Sydney2451 <osiakosyd@gmail.com>
2023-08-22 09:03:25 -0700

    funcs2.c

--- funcs2.c
+++ funcs2.c
@@ -5,11 +5,11 @@
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates the active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: The number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
