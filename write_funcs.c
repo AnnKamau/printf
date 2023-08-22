70e9fb98bf929af635cf7e91fe8b726a4cd78663
Sydney2451 <osiakosyd@gmail.com>
2023-08-22 08:45:56 -0700

    write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - Evaluates if char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
@@ -15,16 +15,16 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ASCCI in hexadecimal code to buffer
  * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * @i: Index at where to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+	/* The hexa format code always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
